4fb70491e0c88e1e984ab9c07d98546b68f83645
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2024-03-05 16:02:31 +0100

    Update pipeline_transform_qgis_resample.py

--- scripts/pipelines/pipeline_transform_qgis_resample.py
+++ scripts/pipelines/pipeline_transform_qgis_resample.py
@@ -39,7 +39,7 @@ from qgis.analysis import QgsNativeAlgorithms
 Processing.initialize()
 
 
-def transform_geomorphon_qgis(dem_file, output_file, excplicit_save = False):
+def transform_geomorphon_qgis(dem_file, output_file, search = 90, skip = 3, flat = 5, dist = 6, excplicit_save = False):
     """
     Function uses GRASS GIS geomorphon algorithm for transforming raster tiles or virtual raster.
     Output is raster file that categorises relief into 8 geomorphology types.
@@ -53,13 +53,12 @@ def transform_geomorphon_qgis(dem_file, output_file, excplicit_save = False):
         return
     else:
         project.addMapLayer(layer)
-        start_time = time.time()
         geomorphon_layer = processing.run("grass7:r.geomorphon", {'elevation':layer,
-                                            'search':90,
-                                            'skip':3,
-                                            'flat':5,
-                                            'dist':6,
-                                            'forms':config["NA_geomorphon_tmp"],
+                                            'search':search,
+                                            'skip':skip,
+                                            'flat':flat,
+                                            'dist':dist,
+                                            'forms':output_file,
                                             '-m':False,
                                             '-e':False,
                                             'GRASS_REGION_PARAMETER':None,
@@ -67,8 +66,6 @@ def transform_geomorphon_qgis(dem_file, output_file, excplicit_save = False):
                                             'GRASS_RASTER_FORMAT_OPT':'',
                                             'GRASS_RASTER_FORMAT_META':''})
         
-        input(f"please press any key to continue, time is {round((time.time()-start_time)/60, 2)}")
-
         if excplicit_save == True:
             # add layer for faster processing
             geomorphon_layer = QgsRasterLayer(geomorphon_layer['forms'], "geomorphon")
@@ -88,11 +85,8 @@ def transform_geomorphon_qgis(dem_file, output_file, excplicit_save = False):
                 provider.extent(),
                 provider.crs()
                 )
-
-        end_time = time.time()
-        total_time = round((end_time-start_time)/60, 2)
-        print(f"Layer processing to geomorphon done! time (dd:hh:mm.ss): {datetime.timedelta(seconds=total_time)} - raw time:({total_time})")
-        print(f"results saved to {output_file}")
+            
+    return
     
 
 def raster_rescale_percent_to_degree(input_raster, output_raster):
@@ -158,9 +152,20 @@ def transform_slope_flow():
 
     print(f"Slope computing and rescaling done in time(dd:hh:mm.ss): {datetime.timedelta(seconds=total_time)} - raw time:({total_time})")
 
-def geomorphon_chunky(central_file, neighbour_vrt, context = QgsProcessingContext(), feedback = QgsProcessingFeedback()):
+def geomorphon_chunky(central_file, neighbour_vrt, output_dir, search = 90, skip = 3, flat = 5, dist = 6):
     """
-    Variation of using geomorphon algorithm in qgis.
+    Function calls geomorphon algorithm in qgis (from grass gis).
+    This is variation of algorithm in qgis - input layers are neighbors DEMs also.
+    
+    Parameters:
+        central_file(str): path to DEM tile for which geomorphon is calculated
+        neighbour_vrt(str): path to created vrt consisting of `central_file` and it's 8 neighbouring tiles
+    
+    Standard parameters are:
+        Search radius = 90
+        Skip radius = 3
+        flatness threshold = 5
+        flatness distance = 6
     """
     project = QgsProject.instance()
     layer = QgsRasterLayer(neighbour_vrt, "Neighbour vrt layer")
@@ -176,11 +181,11 @@ def geomorphon_chunky(central_file, neighbour_vrt, context = QgsProcessingContex
     else:
         project.addMapLayer(layer)
         algorithm_params = {'elevation':layer,
-                            'search':90,
-                            'skip':3,
-                            'flat':5,
-                            'dist':6,
-                            'forms':config["NA_geomorphon_tmp"],
+                            'search':search,
+                            'skip':skip,
+                            'flat':flat,
+                            'dist':dist,
+                            'forms':output_dir,
                             '-m':False,
                             '-e':False,
                             'GRASS_REGION_PARAMETER':None,
@@ -192,7 +197,7 @@ def geomorphon_chunky(central_file, neighbour_vrt, context = QgsProcessingContex
         chunk_extent = central_layer.extent()
 
         # Set the chunk output path
-        chunk_output_path = make_file_path(central_file, config['tmp_dir'], sufix="_geomorphon.tif")
+        chunk_output_path = make_file_path(central_file, output_dir, sufix="_geomorphon.tif")
         algorithm_params['forms'] = chunk_output_path
 
         # Set the extent for the r.geomorphon command
@@ -200,6 +205,8 @@ def geomorphon_chunky(central_file, neighbour_vrt, context = QgsProcessingContex
                                                         f"{chunk_extent.yMinimum()-pixel_size_y*100},{chunk_extent.yMaximum()+pixel_size_y*100}"
 
         # Run the r.geomorphon algorithm for the current dem chunk/tile
+        context = QgsProcessingContext() 
+        feedback = QgsProcessingFeedback()
         geomorphon_result = processing.run('grass7:r.geomorphon', algorithm_params, context=context, feedback=feedback)
         return chunk_output_path
 
