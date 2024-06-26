69864cf3f29801c8cc3954262aebd58be006b0ac
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2024-06-26 09:02:05 +0200

    clean main

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,29 @@
 import sys
 import os
 from pipelines import (
-    pipeline_download_Canada_DEM,
-    pipeline_download_USGS_DEM,
-    pipeline_download_utils_soils,
+    # pipeline_download_Canada_DEM,
+    # pipeline_download_USGS_DEM,
+    # pipeline_download_utils_soils,
     pipeline_transform_vrt_gdal,
     # pipeline_download_WMS_canada,
     pipeline_download_s3_global,
     pipeline_flows,
-    gdal_build_vrt,
-    # pipeline_transform_qgis_resample
+    # pipeline_transform_qgis_resample,
+    pipeline_transform_sea_level,
+    model_data,
+    pixel_conectedness
 )
 import settings
 from qgis_other import qgis_run_app, qgis_create_graphics, qgis_create_grid
+from docs import compile_json_metadata
+# from line_profiler import LineProfiler
+from tqdm import tqdm
+from multiprocessing import Pool
 
 conn_parameters = settings.get_conn_parameters()
 schema = settings.get_schema()
 config = settings.get_config()
+
 
 def main():
     # # Execute download functions
@@ -50,36 +57,38 @@ def main():
     # pipeline_flows.transform_geomorphon_multiprocess_flow()
     # pipeline_flows.transform_geomorphon_vrt_flow()
     # pipeline_flows.transform_tree_cover_density_vrt_flow()
-    pipeline_flows.coastal_flooding_flow()
+    # pipeline_flows.eu_resample_1km_flow()
+    # pipeline_flows.coastal_flooding_30m_NA_flow()
+    # pipeline_flows.coastal_flooding_30m_NA_multiprocessing_flow()
+    # pipeline_flows.coastal_flooding_10m_USA_flow()
+    # pipeline_flows.coastal_flooding_10m_USA_multiprocessing_flow()
+    # pipeline_flows.load_coastal_flooding_30m_to_aws_flow()
+    # pipeline_flows.coastal_flooding_10m_Europe_flow()
+    # pipeline_flows.load_coastal_flooding_10m_EU_to_mnt_flow()
+    # pipeline_flows.load_coastal_flooding_10m_EU_to_aws_flow()
+    pipeline_flows.load_coastal_flooding_10m_USA_to_aws_flow()
 
+    # pipeline_flows.transform_coastal_flooding_vrt()
+    # pipeline_flows.coastal_flooding_flow()
+
+    # pipeline_flows.tree_cover_filter_flow()
+    # pipeline_flows.shrubland_filter_flow()
+    # pipeline_flows.grassland_filter_flow()
+
+    # pipeline_flows.hawaii_repair_flow()
     # pipeline_flows.load_countries_boundaries_flow()
     # pipeline_flows.load_continents_boundaries_flow()
     # pipeline_flows.load_ESA_dem_grid_flow()
+    # pipeline_flows.load_to_mnt_flow()
+    # pipeline_flows.load_to_aws_flow()
 
-    # print(pipeline_download_s3_global.filter_by_continents())
 
-    # creating global dem90 only for usa
-    # pipeline_transform_vrt.ESA_usa_vrt_flow()
-
-    # NA geomorphon
-    # dem_file = config["esa_usa_dem_90_vrt"]
-    # output_file = config["NA_geomorphon"]
-    # output_file = "/mnt/volume-nbg1-1/shared/nikola/NA_geomorphon_N45_E015"
-    # pipeline_transform_resample.transform_geomorphon_qgis(dem_file, output_file)
-
-    # pipeline_transform_vrt.transform_aspect_flow()
-
-    # eu_slope = "/mnt/volume-nbg1-1/satellite/eu_slope/eu_slope_100m_3035.tif"
-    # na_slope = config["NA_slope"]
-    # na_aspect_categorized = config["NA_aspect_categorized"]
-    # dem_90 = config["esa_global_dem_90_vrt"]
-    # esa_world_cover_vrt = config["esa_world_cover_vrt"]
-    # graphical_file = config["tree_cover_density_vrt"]
-
-    # qgis_run_app.grapical_map_flow(graphical_file, zoom = "layer")
-
-    # pipeline_flows.load_aspect_flow()
-    # pipeline_flows.load_slope_flow()
+    # pipeline_flows.coastal_flooding_90m_Europe_flow()
+    # pipeline_flows.coastal_flooding_90m_pixels_flow()
+    # pipeline_flows.coastal_flooding_90m_pixels_multiprocessing_flow()
+    
+    # pipeline_flows.transform_ESA_world_cover_vrt_flow()
+    # compile_json_metadata.main()
 
 if __name__ == "__main__": 
     print("Main ETL script running")
